3220fbd04cb949a2a055213114a2da0bc510e2f2
Aras <114133924+Dirigentas@users.noreply.github.com>
2022-12-02 15:42:40 +0200

    puse darbo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <>
-    <div>labas {console.log('viso')}</div>
-    </>
-  );
+
+    const [juokas, setJuokas] = useState();
+
+    useEffect(() => {
+        fetch('https://v2.jokeapi.dev/joke/Programming?amount=10')
+        .then((res) => res.json())
+        .then((data) => setJuokas(data));
+    }, []);
+
+    return (
+        <div>
+            <div className='container'>
+              {
+                juokas?.jokes.map(j =><div key={j.id}>
+                    <h2>{j.joke}</h2>
+                    <h2>{j.setup} {j.delivery}</h2>
+                </div>)
+              }
+            </div> 
+        </div>
+    );
 }
 
 export default App;
